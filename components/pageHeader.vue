81ac616cdbe5b4175487ed8ff8db5ee9a996d339
Faris Shaik <farisshaik1@gmail.com>
2023-10-20 17:49:48 -0500

    before_commit

--- components/pageHeader.vue
+++ components/pageHeader.vue
@@ -1,13 +1,15 @@
-<!-- components/PageHeader.vue -->
 <template>
   <header class="page-header">
-    <div class="logo">
-      <nuxt-link to="/">Your Logo</nuxt-link>
+    <div class="left">
+      <nuxt-link to="/">
+        <img src="~/../src/assets/mttp_logo.png" alt="Your Logo" />
+      </nuxt-link>
     </div>
-    <nav class="navigation">
-      <nuxt-link to="/about">About</nuxt-link>
-      <nuxt-link to="/contact">Contact</nuxt-link>
-    </nav>
+    <div class="right">
+      <nuxt-link to="/entry">Enter Info</nuxt-link>
+      <nuxt-link to="/view-database">View Database</nuxt-link>
+      <nuxt-link to="/admin">Admin</nuxt-link>
+    </div>
   </header>
 </template>
 
@@ -22,17 +24,24 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
-  color: #fff;
+  background-color: #fff; /* Set the background to white */
+  color: #333; /* Set the text color to dark */
   padding: 10px 20px;
 }
 
-.logo {
-  font-size: 24px;
+.left {
+  display: flex;
+  align-items: center;
 }
 
-.navigation a {
-  color: #fff;
+.left img {
+  width: auto; /* Adjust the image size as needed */
+  height: 40px;
+}
+
+.right a {
+  color: #333; /* Set button text color to dark */
   margin-left: 20px;
+  text-decoration: none;
 }
 </style>
