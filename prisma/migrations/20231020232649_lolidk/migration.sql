aa1c19ed6b263acb77bfd1d808bc3da1c7bf9016
Faris Shaik <farisshaik1@gmail.com>
2023-10-20 21:12:50 -0500

    admin

---
+++ prisma/migrations/20231020232649_lolidk/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `address` on the `Student` table. All the data in the column will be lost.
+  - Added the required column `city` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `county` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `streetAddress` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `zipCode` to the `Student` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Student" (
+    "studentId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "streetAddress" TEXT NOT NULL,
+    "county" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "zipCode" INTEGER NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "voted" BOOLEAN NOT NULL DEFAULT false,
+    "dateCreated" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME,
+    CONSTRAINT "Student_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("userId") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Student" ("authorId", "dateCreated", "firstName", "lastName", "studentId", "updatedAt", "voted") SELECT "authorId", "dateCreated", "firstName", "lastName", "studentId", "updatedAt", "voted" FROM "Student";
+DROP TABLE "Student";
+ALTER TABLE "new_Student" RENAME TO "Student";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
