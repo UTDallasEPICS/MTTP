527a199a921a2d8497373073ecedc6a45fe94b5e
Aryaman Tomer <101023524+AryamanTomer@users.noreply.github.com>
2023-12-02 17:46:44 -0600

    Auth0 Implementation Initial

---
+++ server/api/auth0.ts
@@ -0,0 +1,16 @@
+import {nanoid} from "nanoid"
+
+const state = {}; 
+const genState = () => { const s = nanoid(); state[s] = 1;  return s}
+const runtime = useRuntimeConfig()
+
+export const loginRedirectUrl = () => `${runtime.AUTH0_ISSUER_BASE_URL}authorize?response_type=id_token&response_mode=form_post&client_id=${runtime.AUTH0_CLIENT_ID}&scope=openid%20email&redirect_uri=${encodeURIComponent(runtime.AUTH0_BASE_URL!+"api/callback")}&nonce=${genState()}`
+export const logoutRedirectUrl = (id_token: string) => `${runtime.AUTH0_ISSUER_BASE_URL}oidc/logout?id_token_hint=${id_token}&post_logout_redirect_uri=${encodeURIComponent(runtime.AUTH0_BASE_URL!+"api/logout-complete")}&nonce=${genState()}`
+
+export const verifyNonce = (nonce: string) => {
+    if (state[nonce]) {
+      delete state[nonce]
+      return true
+    }
+    return false
+  }
---
+++ server/middleware/auth.ts
@@ -0,0 +1,56 @@
+import {loginRedirectUrl} from "../api/auth0"
+import jwt from "jsonwebtoken"
+import fs from "fs"
+import { PrismaClient } from "@prisma/client"
+const client = new PrismaClient()
+export default defineEventHandler(async event => {
+  event.context.client = client
+  const cvtoken = getCookie(event, "cvtoken") || ""
+  // not logged in but trying to
+  if (!cvtoken && !(event.node.req.url?.includes('/api/callback') || event.node.req.url?.includes("/Page/") || event.node.req.url?.includes("/api/page") || event.node.req.url?.includes("/"))) {
+    await sendRedirect(event, loginRedirectUrl());
+  } else {
+    // theoretically logged in
+    if (cvtoken) {
+      try {
+        const claims = jwt.verify(
+          cvtoken, 
+          fs.readFileSync(process.cwd()+"/cert-dev.pem")
+        )
+        event.context.claims = claims
+        event.context.user = await event.context.client.user.findFirst(
+          {
+            where:{ email: claims.email }
+          ,
+          include: {
+            Pages: {
+              select: {
+                cuid: true
+              }
+            }
+          }
+          })
+        if(!event.context.user) {
+          console.error(`${claims.email} not found`) 
+          setCookie(event,'cvtoken','')
+          setCookie(event,'cvuser','')
+    
+          return await sendRedirect(event, loginRedirectUrl());
+        }
+        // include pages ids to check if that's the family's page. 
+        setCookie(event, "cvuser", JSON.stringify(event.context.user))
+        /*if(event.context.user.user_role ==='family' && (event.node.req.url?.includes('/EditPage/') || event.node.req.url?.includes('/PageList/'))){
+          if((event.node.req.url?.includes('/EditPage/') && (!(event.node.req.url?.includes('/EditPage/0'))))){
+
+        }
+        }*/
+      } catch (e) {
+        console.error(e) 
+        setCookie(event,'cvtoken','')
+        setCookie(event,'cvuser','')
+    
+        return await sendRedirect(event, loginRedirectUrl())
+      }
+    }
+  }
+})
