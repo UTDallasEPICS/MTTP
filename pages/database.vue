d601d0011c8922ff9a21e39d9853a540df17a0ab
siwonp2 <siwonp2@gmail.com>
2023-12-10 14:25:48 -0600

    doesn't show import/export if the user is a volunteer

--- pages/database.vue
+++ pages/database.vue
@@ -7,9 +7,10 @@
       <PageHeader :userRole="userRole"/>
       
       <h2 class="text-center text-2xl font-bold mt-4">View Database</h2>
-      <h3 class="text-center text-xl font-bold">View the full database and Import/ Export</h3>
-  
-      <div class="flex items-center justify-center sm:col-span-6">
+      <h3 class="text-center text-xl font-bold">View the full database and Import/Export</h3>
+      
+      <!--import and export function-->
+      <div v-if="userRole >= 2" class="flex items-center justify-center sm:col-span-6">
         <input type="file" @change="handleFileSelect" accept=".xlsx, .csv">
         <button type="button" class="rounded-md bg-green-500 px-3 py-2 text-lg font-semibold text-white shadow-sm
         hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
