6721353417590b85c1b95875aa0930eb1e1c05d3
CodyK03 <thecodykerr@gmail.com>
2024-02-28 16:09:27 -0600

    cleaned up admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -8,8 +8,8 @@
         <h2 class="text-center text-2xl font-bold mt-4">Admin Page</h2>
         <h3 class="text-center text-xl font-bold">Modify volunteer and MTTP employee/admin info, and their permissions</h3>
 
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 mx-96">
-          <div class="sm:col-span-3">
+        <div class="mt-10 grid grid-cols-1 gap-x-24 gap-y-8 sm:grid-cols-6 mx-40">
+          <div class="sm:col-span-3 ml-28">
             <label for="first-name" class="block text-lg font-medium leading-6 text-gray-900">First name</label>
             <div class="mt-2">
               <input v-model="user.firstName" type="text" name="first-name" id="first-name" autocomplete="given-name"
@@ -18,7 +18,7 @@
             </div>
           </div>
 
-          <div class="sm:col-span-3">
+          <div class="sm:col-span-3 mr-28">
             <label for="last-name" class="block text-lg font-medium leading-6 text-gray-900">Last name</label>
             <div class="mt-2">
               <input v-model="user.lastName" type="text" name="last-name" id="last-name" autocomplete="family-name"
@@ -27,7 +27,7 @@
             </div>
           </div>
 
-          <div class="sm:col-span-3">
+          <div class="sm:col-span-3 ml-28">
             <label for="email" class="block text-lg font-medium leading-6 text-gray-900">Email</label>
             <div class="mt-2">
               <input v-model="user.email" type="text" name="email" id="email" autocomplete="email" class="block w-full
@@ -36,7 +36,7 @@
             </div>
           </div>
 
-          <div class="sm:col-span-3">
+          <div class="sm:col-span-3 mr-28">
             <label for="role" class="block text-lg font-medium leading-6 text-gray-900">Role</label>
             <div class="mt-2">
               <select v-model="user.role" class="block w-full bg-gray-200 text-gray-700 border rounded-md py-2 px-3
@@ -49,110 +49,117 @@
               </select>
             </div>
           </div>
-            <button type="button" class="text-lg font-semibold leading-6 text-gray-900 mr-3" @click="clearForm">Clear</button>
+            <button type="button" class="text-lg font-semibold leading-6 text-gray-900 mr-3 mx-72" @click="clearForm">Clear</button>
             <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm
             hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-            focus-visible:outline-indigo-600 w-60" @click.prevent="addUser(user)">Submit User</button>
+            focus-visible:outline-indigo-600 w-60 mx-72" @click.prevent="addUser(user)">Submit User</button>
           </div>
         </div>
       </div>
 
       <!--the table used to display the user table-->
-      <div id="table" class="mt-4 mx-96">
+      <div id="table" class="mt-4 mx-40">
         <div class="relative overflow-x-auto rounded-lg">
           <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400 table-fixed">
             <thead class="text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-6 py-3">#</th>
-              <th scope="col" class="px-6 py-3">First Name</th>
-              <th scope="col" class="px-6 py-3">Last Name</th>
-              <th scope="col" class="px-6 py-3">Email</th>
-              <th scope="col" class="px-6 py-3">Role</th>
-              <th scope="col" class="px-6 py-3">Edit</th>
+              <th scope="col" class="py-3">#</th>
+              <th scope="col" class="py-3">First Name</th>
+              <th scope="col" class="py-3">Last Name</th>
+              <th scope="col" class="py-3">Email</th>
+              <th scope="col" class="py-3">Role</th>
+              <th scope="col" class="py-3">Edit</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="(u) in users">
+              <tr v-for="(u) in users" :key="u.userId">
                 <th scope="row">{{ u.userId }}</th>
 
-                <!-- Display first name 
-                      if the edit button is pressed it changes to
-                      an input field to edit (repeated for the rest of the fields as well)-->
-                <td v-if="!editButtonPressed">{{ u.firstName }}</td>
+                <!-- Display first name -->
+                <td v-if="!editButtonPressed">
+                  <div>{{ u.firstName }}</div>
+                </td>
                 <td v-else>
-                  <div v-if="u.userId != editedUser.userId">{{ u.firstName }}</div>
-                  <input v-else type="text" v-model="editedUser.firstName" style="text-align: center; width: 100%;">
+                  <template v-if="u.userId != editedUser.userId">
+                    <div>{{ u.firstName }}</div>
+                  </template>
+                  <template v-else>
+                    <input type="text" v-model="editedUser.firstName" style="text-align: center; width: 100%;">
+                  </template>
                 </td>
 
-                <!-- display last name -->
-                <td v-if="!editButtonPressed">{{ u.lastName }}</td>
+                <!-- Display last name -->
+                <td v-if="!editButtonPressed">
+                  <div>{{ u.lastName }}</div>
+                </td>
                 <td v-else>
-                  <div v-if="u.userId != editedUser.userId">{{ u.lastName }}</div>
-                  <input v-else type="text" v-model="editedUser.lastName" style="text-align: center; width: 100%;">
+                  <template v-if="u.userId != editedUser.userId">
+                    <div>{{ u.lastName }}</div>
+                  </template>
+                  <template v-else>
+                    <input type="text" v-model="editedUser.lastName" style="text-align: center; width: 100%;">
+                  </template>
                 </td>
 
-                <!-- display email-->
-                <td v-if="!editButtonPressed">{{ u.email }}</td>
+                <!-- Display email -->
+                <td v-if="!editButtonPressed">
+                  <div style="overflow: auto;">{{ u.email }}</div>
+                </td>
                 <td v-else>
-                  <div v-if="u.userId != editedUser.userId">{{ u.email }}</div>
-                  <input v-else type="text" v-model="editedUser.email" style="text-align: center; width: 100%;">
+                  <template v-if="u.userId != editedUser.userId">
+                    <div style="overflow: auto;">{{ u.email }}</div>
+                  </template>
+                  <template v-else>
+                    <input type="text" v-model="editedUser.email" style="text-align: center; width: 100%;">
+                  </template>
                 </td>
 
-                <!--
-                    role displays string based on int value
-                -->
-                <div v-if="!editButtonPressed" style="text-align: center; width: 100%;">
-                  <td v-if="u.role == 4">ex-employee</td>
-                  <td v-else-if="u.role == 1">volunteer</td>
-                  <td v-else-if="u.role == 2">employee</td>
-                  <td v-else-if="u.role == 3">admin</td>
-                </div>
-                <div v-else style="text-align: center; width: 100%;">
-                  <div v-if="u.userId != editedUser.userId" >
-                    <td v-if="u.role == 4">ex-employee</td>
-                    <td v-else-if="u.role == 1">volunteer</td>
-                    <td v-else-if="u.role == 2">employee</td>
-                    <td v-else-if="u.role == 3">admin</td>
-                  </div>
-                  <td v-else>
-                    <select class="border-solid border-2" v-model="editedUser.role">
-                            <option value=1>volunteer</option>
-                            <option value=2>employee</option>
-                            <option value=3>admin</option>
-                            <option value=4>ex-employee</option>
-                        </select>
-                  </td>
-                </div>
+                <!-- Display role -->
+                <td>
+                  <template v-if="!editButtonPressed">
+                    <div style="text-align: center; width: 100%;">
+                      <span v-if="u.role == 4">ex-employee</span>
+                      <span v-else-if="u.role == 1">volunteer</span>
+                      <span v-else-if="u.role == 2">employee</span>
+                      <span v-else-if="u.role == 3">admin</span>
+                    </div>
+                  </template>
+                  <template v-else>
+                    <div v-if="u.userId != editedUser.userId" style="text-align: center; width: 100%;">
+                      <span v-if="u.role == 4">ex-employee</span>
+                      <span v-else-if="u.role == 1">volunteer</span>
+                      <span v-else-if="u.role == 2">employee</span>
+                      <span v-else-if="u.role == 3">admin</span>
+                    </div>
+                    <template v-else>
+                      <select class="border-solid border-2" v-model="editedUser.role">
+                        <option value="1">volunteer</option>
+                        <option value="2">employee</option>
+                        <option value="3">admin</option>
+                        <option value="4">ex-employee</option>
+                      </select>
+                    </template>
+                  </template>
+                </td>
 
-              <td>
-                <!--the edit button
-                    Appears when the row is not in edit mode
-                    when clicked the user in the row is stored in editedUser
-                    then modified in the input fields that show up-->
-                <button id="editUserButton" class="rounded-md bg-indigo-600 px-3 py-2 text-xs font-semibold text-white shadow-sm
-            hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-            focus-visible:outline-indigo-600" v-if="!editButtonPressed" @click="{editedUser.userId = u.userId; 
-                                                    editedUser.firstName = u.firstName; 
-                                                    editedUser.lastName = u.lastName;
-                                                    editedUser.email = u.email;
-                                                    editedUser.role = u.role;
-                                                    editButtonPressed = true;}">Edit</button>
-                <!--when the edit button is pressed
-                    user can choose to apply the edit or cancel-->
-                <div v-else>
-                  <div v-if="editedUser.userId == u.userId">
+                <!-- Edit button -->
+                <td>
+                  <button id="editUserButton" class="rounded-md bg-indigo-600 px-3 py-2 text-xs font-semibold text-white shadow-sm
+                        hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                        focus-visible:outline-indigo-600" v-if="!editButtonPressed" @click="editButtonPressed = true; setEditedUser(u)">Edit</button>
+                  <!-- Apply/Cancel buttons -->
+                  <div v-else>
                     <button id="applyEditButton" class="rounded-md bg-indigo-600 px-3 py-2 text-xs font-semibold text-white shadow-sm
-            hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-            focus-visible:outline-indigo-600" @click="{editButtonPressed = false;
-                                                          editUser(editedUser);}">Apply</button>
+                        hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                        focus-visible:outline-indigo-600" @click="applyEdit(u)">Apply</button>
                     <button id="cancelEditButton" class="rounded-md bg-gray-600 px-3 py-2 text-xs font-semibold text-white shadow-sm
-            hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-            focus-visible:outline-indigo-600" @click="editButtonPressed = false">Cancel</button>
+                        hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                        focus-visible:outline-indigo-600" @click="cancelEdit">Cancel</button>
                   </div>
-                </div>
-              </td>
-            </tr>
+                </td>
+              </tr>
             </tbody>
+
           </table>
         
         </div>
