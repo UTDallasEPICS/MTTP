fe67a496a7d469697ce657c00ea4da69c6c63582
jahnavi <jahnaviy13@gmail.com>
2024-03-27 15:51:37 -0500

    reset db

--- prisma/migrations/20240327205040_init/migration.sql
+++ prisma/migrations/20240327205040_init/migration.sql
@@ -1,6 +1,5 @@
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_Student" (
+-- CreateTable
+CREATE TABLE "Student" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
@@ -18,8 +17,17 @@ CREATE TABLE "new_Student" (
     "schoolName" TEXT NOT NULL,
     CONSTRAINT "Student_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
-INSERT INTO "new_Student" ("authorId", "city", "county", "dateCreated", "firstName", "id", "lastName", "phoneNumber", "schoolName", "streetAddress", "streetNumber", "studentEmail", "updatedAt", "voted", "zipCode") SELECT "authorId", "city", "county", "dateCreated", "firstName", "id", "lastName", "phoneNumber", "schoolName", "streetAddress", "streetNumber", "studentEmail", "updatedAt", "voted", "zipCode" FROM "Student";
-DROP TABLE "Student";
-ALTER TABLE "new_Student" RENAME TO "Student";
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "dateCreated" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
