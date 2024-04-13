7df65fa6787142c3bbab6b32705e5fe628218a1a
CodyK03 <thecodykerr@gmail.com>
2024-04-13 15:32:40 -0500

    Entry fields now auto clear when you add a user

--- pages/admin.vue
+++ pages/admin.vue
@@ -51,7 +51,7 @@
           <button type="button" class="text-lg font-semibold leading-6 text-gray-900 mr-3 mx-72" @click="clearForm">Clear</button>
           <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm
           hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-          focus-visible:outline-indigo-600 w-60 mx-72" @click.prevent="addUser(user)">Submit User</button>
+          focus-visible:outline-indigo-600 w-60 mx-72" @click.prevent="addUser(user); clearForm()">Submit User</button>
         </div>
       </div>
     </div>
