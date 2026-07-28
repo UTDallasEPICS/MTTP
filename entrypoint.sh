e890b398dd6b9cc338e293eac3779ff2ee8f60bc
fegelsamuel <fegelsamuel@gmail.com>
2026-07-27 20:06:16 -0500

    don't need seeding; commented out pnpm prisma db seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 # Apply migrations and initialize migrations if it does not exist
 pnpm prisma generate
 pnpm prisma migrate deploy
-pnpm prisma db seed
+# Commenting out because we aren't seeding anything
+# pnpm prisma db seed
 
 # Run the CMD command from the dockerfile
 exec "$@"
